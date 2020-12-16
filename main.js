47fc2397993e1865ff5dc0e02c9241c1ac3bc0f0
lexie-kaia <73787590+lexie-kaia@users.noreply.github.com>
2020-12-16 16:31:54 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -48,8 +48,23 @@ const homeHeight = home.getBoundingClientRect().height;
 const homeContainer = document.querySelector('.home__container')
 
  document.addEventListener('scroll', () => {
-  if(1 - window.scrollY / homeHeight < 0) {
-    return;
-  }
   homeContainer.style.opacity = 1 - window.scrollY / homeHeight
  });
+
+
+ // show 'arrow up' button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+})
+
+ // Handle click on the 'arrow up' button
+
+ arrowUp.addEventListener('click', () => {
+   scrollIntoView('#home');
+ })
